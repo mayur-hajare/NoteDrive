6675d8c9a320f12fe22e61d80fcb4724a7bf6c53
mayur-hajare <myur.hajare@gmail.com>
2021-05-31 13:02:08 +0530

    All set But data not Upload....

--- app/src/main/java/com/myur/notedrive/CreateNoteActivity.java
+++ app/src/main/java/com/myur/notedrive/CreateNoteActivity.java
@@ -1,14 +1,99 @@
 package com.myur.notedrive;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateNoteActivity extends AppCompatActivity {
+
+    EditText edTitle, edContent;
+    FloatingActionButton msavebtn;
+
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_note);
+
+        msavebtn = findViewById(R.id.saveNote);
+        edTitle = findViewById(R.id.TitleOfNote);
+        edContent = findViewById(R.id.contentOfNote);
+
+        Toolbar toolbar = findViewById(R.id.tooleBarCn);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        msavebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = edTitle.getText().toString();
+                String content = edContent.getText().toString();
+                Toast.makeText(getApplicationContext(), title, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), content, Toast.LENGTH_SHORT).show();
+
+                if (title.isEmpty() || content.isEmpty()) {
+
+                    Toast.makeText(getApplicationContext(), "Both field are Required...", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    DocumentReference documentReference = firebaseFirestore.collection("notes").document(firebaseUser.getUid()).collection("myNotes").document();
+                    Map<String, Object> note = new HashMap<>();
+                    note.put("title", title);
+                    note.put("content", content);
+
+                    documentReference.set(note).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(getApplicationContext(), "Note Created Successfully...", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(CreateNoteActivity.this, MainActivity.class));
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(), "Field To Create Note...", Toast.LENGTH_SHORT).show();
+                            //startActivity(new Intent(CreateNoteActivity.this,MainActivity.class));
+                        }
+                    });
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
